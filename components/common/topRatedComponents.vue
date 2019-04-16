6eed3ee819e3cd43aa0835cc7d661ad5b1686686
alekseyjf <morhun1993@gmail.com>
2019-04-16 09:39:08 +0300

    !fix: component favorite

--- components/common/topRatedComponents.vue
+++ components/common/topRatedComponents.vue
@@ -19,7 +19,8 @@
         <nuxt-link :to="'film/'+filmId">
           <v-btn flat>detail</v-btn>
         </nuxt-link>
-        <v-btn color="primary" @click="addFavorite">add favorites</v-btn>
+        <v-btn v-if="flag" color="primary" @click="addFavorite">add favorites</v-btn>
+        <v-btn v-else color="primary" @click="removeFavorite">remove favorites</v-btn>
       </v-card-actions>
     </v-card>
   </v-flex>
@@ -30,7 +31,8 @@
     name: "top_rated",
     data() {
       return {
-        imageUrl: 'https://image.tmdb.org/t/p/w500/'+this.isImage
+        imageUrl: 'https://image.tmdb.org/t/p/w500/'+this.isImage,
+        flag: true
       }
     },
     props: ['title', 'desc', 'filmId', 'isImage'],
@@ -39,6 +41,19 @@
         console.log(this.filmId);
         this.$store.dispatch('setFavorite', this.filmId)
         console.log('this.$store.getters.retFavoritesList', this.$store.getters.retFavoritesList);
+        this.flag = !this.flag;
+      },
+      removeFavorite() {
+        console.log(this.filmId);
+        console.log(this.$store.getters.retFavoritesList.length);
+        this.flag = !this.flag;
+        for(let i=0; i < this.$store.getters.retFavoritesList.length; i++) {
+          console.log(this.$store.getters.retFavoritesList[i],  this.filmId);
+          if(this.$store.getters.retFavoritesList[i] == this.filmId) {
+
+            console.log('this.filmId', this.filmId);
+          }
+        }
       }
     }
   }
